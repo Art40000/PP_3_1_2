4e670be1a53ed78054955e90cf40c2f7d82eb8ba
Art40000 <artemirii@gmail.com>
2022-11-21 19:43:13 +0300

    C2

--- src/main/java/com/example/crudmvcboot/service/UserService.java
+++ src/main/java/com/example/crudmvcboot/service/UserService.java
@@ -4,6 +4,7 @@ import com.example.crudmvcboot.model.User;
 import com.example.crudmvcboot.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,19 +17,20 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    @Transactional
     public void saveUser(User user) {
         userRepository.save(user);
     }
-
+    @Transactional
     public void remove(Long id) {
         userRepository.deleteById(id);
     }
 
-
+    @Transactional
     public User getUserById(Long id) {
         return userRepository.findById(id).get();
     }
-
+    @Transactional
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
